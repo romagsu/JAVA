55b8cea49e94798ea0dc3cc5eaecba400140f5d2
RomXs <47647228+RomXs@users.noreply.github.com>
2019-04-11 05:01:32 +0300

    Update Runner.java
    Fixed 2

--- lab1/src/Runner.java
+++ lab1/src/Runner.java
@@ -1,9 +1,12 @@
+import by.gsu.pmslab.Byn;
 import by.gsu.pmslab.Product;
 
 import java.util.Arrays;
 import java.util.Comparator;
 
 public class Runner {
+    
+    public Byn toByn;
 
     public static void main(String[] args) {
         Product[] products = {
@@ -14,7 +17,7 @@ public class Runner {
                 new Product("Tomato", true, 125),
                 new Product("Bread", true, 98),
                 new Product("Eggs", true, 128),
-                new Product("Potato", true, 264),
+                null,
                 new Product("Butter", false, 223),
                 new Product("Pasta", true, 142),
         };
@@ -25,6 +28,15 @@ public class Runner {
         Arrays.sort(products, new Comparator<Product>() {
             @Override
             public int compare(Product o1, Product o2) {
+                if (o1 == null && o2 == null) {
+                    return 0;
+                }
+                if (o1 == null) {
+                    return 1;
+                }
+                if (o2 == null) {
+                    return -1;
+                }
                 return o1.getPrice() - o2.getPrice();
             }
         });
@@ -34,6 +46,15 @@ public class Runner {
         Arrays.sort(products, new Comparator<Product>() {
             @Override
             public int compare(Product o1, Product o2) {
+                if (o1 == null && o2 == null) {
+                    return 0;
+                }
+                if (o1 == null) {
+                    return 1;
+                }
+                if (o2 == null) {
+                    return -1;
+                }
                 return o1.getName().compareTo(o2.getName());
             }
         });
@@ -42,29 +63,35 @@ public class Runner {
 
         int sum=+countPrice(products);
 
-        System.out.println("\n\tTotal cost product = "+ by.gsu.pmslab.Byn.toBYN(sum));
+        System.out.println("\n\tTotal cost product = "+ Byn.toBYN(sum));
 
         System.out.println("\tProduct in stock = "+countStock(products));
     }
 
     private static void show(Product[] array){ // method show
         for (Product item : array){
-            System.out.println(item);
+            if (item != null) {
+                System.out.println(item);
+            }    
         }
     }
     private static int countPrice(Product[] array){ // method total cost
         int cents = 0;
         for (Product item : array){
-            cents += item.getPrice();
+            if (item != null) {
+                cents += item.getPrice();
+            }
         }
         return cents;
     }
     private static int countStock(Product[] array){ // method product in stock
         int num = 0;
         for (Product item : array){
-            if (item.isStock()){
-                num++;
-            }
+            if (item != null) {
+                if (item.isStock()){
+                    num++;
+                }
+            }    
         }
         return num;
     }
